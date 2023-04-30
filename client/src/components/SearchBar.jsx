805791d9abc0bc4e7114a65be0c57411c24bd442
Anthony Wu <52024770+anthonywu2000@users.noreply.github.com>
2023-04-30 10:51:59 -0700

    Update SearchBar.jsx

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 import { Search as SearchIcon } from '@material-ui/icons';
 import { InputBase, IconButton } from "@material-ui/core";
 
+// creates the search bar with search icon on the left of the input field
 const SearchBar = ({filter, func}) => {
     return (
         <div className="filter-container">
