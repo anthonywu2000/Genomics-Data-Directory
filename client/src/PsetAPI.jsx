c7b631df76cc9c02737c74d6f64c0002886602bb
Anthony Wu <52024770+anthonywu2000@users.noreply.github.com>
2023-04-30 11:05:08 -0700

    Update PsetAPI.jsx

--- client/src/PsetAPI.jsx
+++ client/src/PsetAPI.jsx
@@ -3,6 +3,7 @@ import Loader from "./components/Loader";
 import usePsets from "./utils/usePsets";
 import Display from "./Display.jsx";
 
+// creates the page which contains the information from the ORCESTRA API
 const PsetAPI = () => {
   const { filter, filteredPsets, isLoading, handleFilterChange } = usePsets('http://localhost:3000/api/psets');
 
