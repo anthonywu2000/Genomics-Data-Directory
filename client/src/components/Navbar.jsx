a9e70e0a87891113b264bc8fa510ec75d5c72ed7
Anthony Wu <52024770+anthonywu2000@users.noreply.github.com>
2023-04-30 15:08:22 -0700

    update navbar linking

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,17 +2,17 @@ import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 // creates the navigation bar that links between the two pages
-const Navbar = ({title, link1, link2}) => {
+const Navbar = ({title, link1, link2, ref1, ref2}) => {
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" style={{ flexGrow: 1 }}>
           {title}
         </Typography>
-        <Button color="inherit" component={Link} to="/">
+        <Button color="inherit" component={Link} to={ref1}>
           {link1}
         </Button>
-        <Button color="inherit" component={Link} to="/database">
+        <Button color="inherit" component={Link} to={ref2}>
           {link2}
         </Button>
       </Toolbar>
