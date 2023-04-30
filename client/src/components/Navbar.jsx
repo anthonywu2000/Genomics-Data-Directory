15e06fa1c6a7c793da03216a4b11bed321b78233
Anthony Wu <52024770+anthonywu2000@users.noreply.github.com>
2023-04-30 10:49:13 -0700

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
+// creates the navigation bar that links between the two pages
 const Navbar = ({title, link1, link2}) => {
   return (
     <AppBar position="static">
