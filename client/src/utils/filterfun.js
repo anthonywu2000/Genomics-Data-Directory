f6e7cfdf8c39ff88cdfe07312170052b6e630a20
Anthony Wu <52024770+anthonywu2000@users.noreply.github.com>
2023-04-30 10:56:51 -0700

    Update filterfun.js

--- client/src/utils/filterfun.js
+++ client/src/utils/filterfun.js
@@ -1,3 +1,10 @@
+/**
+ * Filters an array of objects based on a given filter string.
+ * @param {Array} dat - An array of objects to be filtered.
+ * @param {string} filter - A string used to filter objects based on their name property.
+ * @returns {Array} - An array of objects that contain the filter string in their name property, case-insensitive.
+ */
+
 const filterfun = (dat, filter) => {
     return dat.filter(d => d.name.toLowerCase().includes(filter.toLowerCase()));
 }
